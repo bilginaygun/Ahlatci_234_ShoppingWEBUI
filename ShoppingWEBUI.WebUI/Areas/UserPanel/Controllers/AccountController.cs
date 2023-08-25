2f540e119d9ccf0e10ff9e6d06995c118d19425a
bilgi <bilgi@DESKTOP-4G3GV4F>
2023-08-25 12:57:53 +0300

    Add project files.

---
+++ ShoppingWEBUI.WebUI/Areas/UserPanel/Controllers/AccountController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RestSharp;
+using ShoppingWEBUI.Core.DTO;
+
+namespace ShoppingWEBUI.WebUI.Areas.UserPanel.Controllers
+{
+    [Area("UserPanel")]
+    public class AccountController : Controller
+    {
+
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        public AccountController(IHttpContextAccessor contextAccessor)
+        {
+            _httpContextAccessor = contextAccessor;
+        }
+
+
+        [HttpGet("/User/Register")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost("/Account/UserRegister")]
+
+       
+        public async Task<ActionResult> LoginAsync(LoginDTO loginDTO)
+        {
+            var url = "http://localhost:5183/Login";
+            var client = new RestClient(url);
+            var request = new RestRequest(url, Method.Post);
+            request.AddHeader("Content-Type", "application/json");
+            var body = JsonConvert.SerializeObject(loginDTO);
+            request.AddBody(body,"application/json");
+            RestResponse restResponse = await client.ExecuteAsync(request);
+            var responseObject = restResponse.Content;
+            return RedirectToAction("Index", "Home");
+
+
+
+        }
+
+    }
+}
---
+++ ShoppingWEBUI.WebUI/Areas/UserPanel/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShoppingWEBUI.Core.ViewModel;
+using ShoppingWEBUI.Helper.SessionHelper;
+
+namespace ShoppingWEBUI.WebUI.Areas.UserPanel.Controllers
+{
+    [Area("UserPanel")]
+    public class HomeController : Controller
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public HomeController(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+        [HttpGet("/User/Anasayfa")]
+        public IActionResult Index()
+        {
+            UserViewModel user = new()
+            {
+
+                AdSoyad = SessionManager.LoggedUser.AdSoyad,
+                ID = Convert.ToInt32(SessionManager.LoggedUser.UserID),
+                EPosta = SessionManager.LoggedUser.EPosta
+
+            };
+
+            return View(user);
+        }
+    }
+}
+
